4503ac748aaf2d5d8d71647a3cd17a21f9200f6a
milman2 <milman2@voyagergames.gg>
2025-09-30 13:12:00 +0900

    AsioClient 클래스 구현: 비동기 소켓 통신을 위한 AsioClient 클래스를 새로 추가하여 클라이언트 연결 관리, 패킷 송수신 및 연결 해제를 처리하도록 구현함. AsioServer와의 통신을 위한 메서드 및 패킷 처리 로직을 포함함.

--- server/Network/Game/AsioServer.h
+++ server/Network/Game/AsioServer.h
@@ -24,12 +24,12 @@
 
 #include "PacketProtocol.h"
 #include "BT/BehaviorTreeEngine.h"
+#include "AsioClient.h"
 
 namespace bt
 {
 
     // 전방 선언
-    class AsioClient;
     class BehaviorTreeEngine;
     class MonsterBTExecutor;
     class MonsterManager;
@@ -177,57 +177,5 @@ namespace bt
         mutable boost::mutex log_mutex_;
     };
 
-    // Asio 기반 클라이언트 클래스
-    class AsioClient : public boost::enable_shared_from_this<AsioClient>
-    {
-    public:
-        AsioClient(boost::asio::io_context& io_context, AsioServer* server);
-        ~AsioClient();
-
-        // 연결 관리
-        void Start();
-        void Stop();
-        bool IsConnected() const { return connected_.load(); }
-
-        // 패킷 송수신
-        void SendPacket(const Packet& packet);
-        void ReceivePacket();
-
-        // 소켓 접근
-        boost::asio::ip::tcp::socket&       Socket() { return socket_; }
-        const boost::asio::ip::tcp::socket& Socket() const { return socket_; }
-
-        // 클라이언트 정보
-        std::string                             GetIPAddress() const;
-        uint16_t                                GetPort() const;
-        boost::chrono::steady_clock::time_point GetConnectTime() const { return connect_time_; }
-
-    private:
-        // 패킷 처리
-        void HandlePacketSize(const boost::system::error_code& error, size_t bytes_transferred);
-        void HandlePacketData(const boost::system::error_code& error, size_t bytes_transferred);
-        void HandleSend(const boost::system::error_code& error, size_t bytes_transferred);
-
-        // 연결 관리
-        void HandleDisconnect();
-
-    private:
-        boost::asio::io_context&     io_context_;
-        boost::asio::ip::tcp::socket socket_;
-        AsioServer*                  server_;
-
-        std::atomic<bool>                       connected_;
-        boost::chrono::steady_clock::time_point connect_time_;
-
-        // 패킷 수신 버퍼
-        boost::array<uint8_t, 4096> receive_buffer_;
-        uint32_t                    expected_packet_size_;
-        std::vector<uint8_t>        packet_buffer_;
-
-        // 패킷 전송 큐
-        std::queue<Packet> send_queue_;
-        boost::mutex       send_queue_mutex_;
-        bool               sending_;
-    };
 
 } // namespace bt
